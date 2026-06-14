2e4277a3d2ca976011cdc960352e633abdb2bd42
lurzard <lurienthelizard@gmail.com>
2026-06-14 02:25:31 -0400

    modify home

---
+++ public/navbar/navbar.js
@@ -0,0 +1,7 @@
+document.getElementById("navbar").innerHTML =
+    '<p class="mute">>PROJECT: PITCH BLACK</p>' +
+    '<a id="logo" href="pages/home.html">THE MOD</a>' +
+    '<a class="mute" href="pages/ost.html">SOUNDTRACK</a>' +
+    '<a class="mute" href="pages/news.html">NEWS</a>' +
+    '<a class="mute" href="pages/faq.html">FAQ</a>'
+;
