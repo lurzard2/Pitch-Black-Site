9aec6cf888727e2e749a7dfaa412eed3f7665e30
lurzard <lurienthelizard@gmail.com>
2026-06-14 15:50:48 -0400

    modify nav for chat

--- public/navbar/navbar.js
+++ public/navbar/navbar.js
@@ -7,5 +7,5 @@ document.getElementById("navbar").innerHTML =
     '<a class="mute" href="../pages/ost.html">SOUNDTRACK</a>' +
     '<a class="mute" href="../pages/news.html">NEWS</a>' +
     '<a class="mute" href="../pages/faq.html">FAQ</a>' +
-    '<div id="chat"><iframe width="100%" height="100%" src="https://www3.cbox.ws/box/?boxid=3548317&boxtag=w5ovQB" allowtransparency="yes" allow="autoplay" frameborder="0" marginheight="0" marginwidth="0" scrolling="auto"></iframe></div>'
+    '<div id="chat-container"><iframe id="chat" width="100%" height="100%" src="https://www3.cbox.ws/box/?boxid=3548317&boxtag=w5ovQB" allowtransparency="yes" allow="autoplay" frameborder="0" marginheight="0" marginwidth="0" scrolling="auto"></iframe></div>'
 ;
