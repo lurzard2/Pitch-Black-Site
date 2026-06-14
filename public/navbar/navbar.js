480b531513a601e5df5d83005c2f18d3de3ae248
lurzard <lurienthelizard@gmail.com>
2026-06-14 15:34:23 -0400

    modify nav for chat

--- public/navbar/navbar.js
+++ public/navbar/navbar.js
@@ -1,11 +1,11 @@
 document.getElementById("navbar").innerHTML =
     '<div id="statuscafe"><div id="statuscafe-username"></div><div id="statuscafe-content"></div></div>' +
-    '' +
     '<p>NAVIGATION</p>' +
     '<a href="../pages/home.html">HOME</a>' +
     '<p>PROJECT: PITCH BLACK</p>' +
     '<a href="../pages/mod.html">THE MOD</a>' +
     '<a class="mute" href="../pages/ost.html">SOUNDTRACK</a>' +
     '<a class="mute" href="../pages/news.html">NEWS</a>' +
-    '<a class="mute" href="../pages/faq.html">FAQ</a>'
+    '<a class="mute" href="../pages/faq.html">FAQ</a>' +
+    '<div id="chat"><iframe width="100%" height="100%" src="https://www3.cbox.ws/box/?boxid=3548317&boxtag=w5ovQB" allowtransparency="yes" allow="autoplay" frameborder="0" marginheight="0" marginwidth="0" scrolling="auto"></iframe></div>'
 ;
