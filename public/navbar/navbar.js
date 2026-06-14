a86db3ddf24bd55479f1b2980fe2775317b091c4
lurzard <lurienthelizard@gmail.com>
2026-06-14 03:21:14 -0400

    modify nav

--- public/navbar/navbar.js
+++ public/navbar/navbar.js
@@ -1,8 +1,9 @@
 document.getElementById("navbar").innerHTML =
-    '<p>NAVIGATION</p>'
-    '<p class="mute">>PROJECT: PITCH BLACK</p>' +
-    '<a href="mod.html">THE MOD</a>' +
-    '<a class="mute" href="pages/ost.html">SOUNDTRACK</a>' +
-    '<a class="mute" href="pages/news.html">NEWS</a>' +
-    '<a class="mute" href="pages/faq.html">FAQ</a>'
+    '<p>>NAVIGATION</p>' +
+    '<a href="../pages/home.html"></a>' +
+    '<p>>PROJECT: PITCH BLACK</p>' +
+    '<a href="../pages/mod.html">THE MOD</a>' +
+    '<a class="mute" href="../pages/ost.html">SOUNDTRACK</a>' +
+    '<a class="mute" href="../pages/news.html">NEWS</a>' +
+    '<a class="mute" href="../pages/faq.html">FAQ</a>'
 ;
