652f5a05b5cf54646cd7e2a804a7713d9ffda8dc
lurzard <lurienthelizard@gmail.com>
2026-06-14 01:34:01 -0400

    modify home (remove iframe)

---
+++ public/navbar.js
@@ -0,0 +1,7 @@
+document.getElementById("navbar").innerHTML = 
+    '<p class="mute">NAVIGATION</p>'+
+    '<a id="logo" href="pages/home.html">HOME</a>'+
+    '<a class="mute" href="pages/ost.html">SOUNDTRACK</a>'+
+    '<a class="mute" href="pages/news.html">NEWS</a>'+
+    '<a class="mute" href="pages/faq.html">FAQ</a>'
+;
