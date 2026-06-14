714e1f1de5a4cac2660bbb3f690e1869d35c6e80
lurzard <lurienthelizard@gmail.com>
2026-06-14 03:19:13 -0400

    modify all

--- public/navbar/navbar.js
+++ public/navbar/navbar.js
@@ -1,6 +1,7 @@
 document.getElementById("navbar").innerHTML =
+    '<p>NAVIGATION</p>'
     '<p class="mute">>PROJECT: PITCH BLACK</p>' +
-    '<a href="index.html">THE MOD</a>' +
+    '<a href="mod.html">THE MOD</a>' +
     '<a class="mute" href="pages/ost.html">SOUNDTRACK</a>' +
     '<a class="mute" href="pages/news.html">NEWS</a>' +
     '<a class="mute" href="pages/faq.html">FAQ</a>'
