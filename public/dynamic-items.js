49188de5e751a58c9705013a855542e66975250d
lurzard <lurienthelizard@gmail.com>
2026-06-15 09:27:29 -0400

    modify mod

--- public/dynamic-items.js
+++ public/dynamic-items.js
@@ -1,14 +1,14 @@
 document.getElementById("nav-container").innerHTML =
     '<div id="nav-links">' +
-    '<p>NAVIGATION</p>' +
+    '<p>>NAVIGATION</p>' +
     '<a href="../pages/home.html">HOME</a>' +
-    '<p>PROJECT: PITCH BLACK</p>' +
+    '<p>>PROJECT: PITCH BLACK</p>' +
     '<a href="../pages/mod.html">THE MOD</a>' +
     '<a class="mute" href="../pages/ost.html">SOUNDTRACK</a>' +
     '<a class="mute" href="../pages/news.html">NEWS</a>' +
     '<a class="mute" href="../pages/faq.html">FAQ</a>' +
     '</div>' +
-    '<div id="nav-chat"><p>live chat</p><iframe id="chat" width="100%" height="100%" src="https://www3.cbox.ws/box/?boxid=3548317&boxtag=w5ovQB" allowtransparency="yes" allow="autoplay" frameborder="0" marginheight="0" marginwidth="0" scrolling="auto"></iframe></div>'
+    '<div id="nav-chat"><iframe id="chat" width="100%" height="100%" src="https://www3.cbox.ws/box/?boxid=3548317&boxtag=w5ovQB" allowtransparency="yes" allow="autoplay" frameborder="0" marginheight="0" marginwidth="0" scrolling="auto"></iframe></div>'
 ;
 
 document.getElementById('header').innerHTML =
