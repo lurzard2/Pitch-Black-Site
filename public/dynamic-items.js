5f3108bb3f5469e5384ab3e427e3e3259dc5cdd4
lurzard <lurienthelizard@gmail.com>
2026-06-15 06:57:53 -0400

    reworked layout (desktop)

---
+++ public/dynamic-items.js
@@ -0,0 +1,16 @@
+document.getElementById("nav-container").innerHTML =
+    '<div id="nav-links">' +
+    '<p>NAVIGATION</p>' +
+    '<a href="../pages/home.html">HOME</a>' +
+    '<p>PROJECT: PITCH BLACK</p>' +
+    '<a href="../pages/mod.html">THE MOD</a>' +
+    '<a class="mute" href="../pages/ost.html">SOUNDTRACK</a>' +
+    '<a class="mute" href="../pages/news.html">NEWS</a>' +
+    '<a class="mute" href="../pages/faq.html">FAQ</a>' +
+    '</div>' +
+    '<div id="nav-chat"><iframe id="chat" width="100%" height="100%" src="https://www3.cbox.ws/box/?boxid=3548317&boxtag=w5ovQB" allowtransparency="yes" allow="autoplay" frameborder="0" marginheight="0" marginwidth="0" scrolling="auto"></iframe></div>'
+;
+
+document.getElementById('header').innerHTML =
+    '<div id="statuscafe"><div id="statuscafe-username"></div><div id="statuscafe-content"></div></div>'
+    ;
