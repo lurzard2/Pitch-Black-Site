65a344b933a101c4014a9261b5743aed3fc98e49
lur <lurienthelizard@gmail.com>
2026-08-16 21:16:16 -0400

    prototype save system

---
+++ public/SaveData.js
@@ -0,0 +1,54 @@
+//deadname
+//deadpronouns
+//name
+//pronouns
+//favdrink
+//siblingname
+
+const deadname = "deadname";
+const name = "name";
+const deadpronoun = "deadpronoun";
+const pronouns = "pronouns";
+
+const Path = {
+    LOCAL: "local",
+    SESSION: "session",
+}
+
+function cantGetStorage(){ return typeof (Storage) == "undefined"; }
+
+function trySaveLocal(key, value) {
+    modify(true, key, value, Path.LOCAL)
+}
+
+function trySaveSession(key, value){
+    modify(true, key, value, Path.SESSION);
+}
+
+function tryLoadLocal(key, value) {
+    modify(false, key, value, Path.LOCAL)
+}
+function tryLoadSession(key, value) {
+    modify(false, key, value, Path.SESSION);
+}
+
+function modify(flag, key, value, path) {
+    if (cantGetStorage()) {
+        return;
+    }
+    if (flag) {
+        if (path === Path.LOCAL) {
+            localStorage.setItem(key, value);
+        }
+        if (path === Path.SESSION) {
+            sessionStorage.setItem(key, value);
+        }
+    } else {
+        if (path === Path.LOCAL) {
+            sessionStorage.setItem(key, value);
+        }
+        if (path === Path.SESSION) {
+            sessionStorage.setItem(key, value);
+        }
+    }
+}
