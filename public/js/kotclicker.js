cfa1707528426c659f1e977e4a70cb4df537a8d2
lur <lurienthelizard@gmail.com>
2026-08-26 16:26:00 -0400

    kot clicker counter (fix nan)

--- public/js/kotclicker.js
+++ public/js/kotclicker.js
@@ -1,11 +1,10 @@
 import {loadraw, saveraw, exists, save} from './savedata.js';
 import { Data } from './savedata.js'
-import { button } from './themeswitcher.js';
 
-document.querySelector('.counter').innerHTML = loadraw(Data.KotClickerCounter);
+document.querySelector('.counter').innerHTML = loadraw(Data.KotClickerCounter) || 0;
 
 export function increment(){
-    let count = exists(Data.KotClickerCounter) ? parseInt(loadraw(Data.KotClickerCounter)) : saveraw(Data.KotClickerCounter, 0);
+    let count = parseInt(loadraw(Data.KotClickerCounter)) || 0;
     count += 1;
     document.querySelector('.counter').innerHTML = count;
     saveraw(Data.KotClickerCounter, count);
