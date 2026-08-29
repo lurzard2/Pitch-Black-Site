d2caa6dfd5070b5f96089e51e3c638feb1c80f98
lur <lurienthelizard@gmail.com>
2026-08-29 00:00:54 -0400

    note for dataminers

---
+++ js/_NOTEFORYOU.js
@@ -0,0 +1,23 @@
+/*
+
+Hi.
+Yes the site's 'savefile' is saved on the client.
+This is a singleplayer game and your progress is stored locally.
+
+By viewing the source code you acknowledge your experience will be permanently affected.
+Neither positive, nor negative,
+Just affected.
+
+These experiences are intended to be experienced BLIND.
+Viewing internal functioning could expose things otherwise not meant to be seen.
+Or possibly, hidden for you to find...
+I ask you wait until you are ready to proceed, to do this.
+
+BE WARY OF SPOILERS.
+DO NOT LEAK.
+DO NOT SABOTAGE.
+PLEASE ENJOY WHAT IS HERE IN A FAITHFUL WAY.
+AND HAVE FUN.
+THAT IS ALL.
+
+ */
