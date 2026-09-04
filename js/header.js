40d9fba233beae416d90faeb6de8ab941302c259
lur <lurienthelizard@gmail.com>
2026-09-04 11:39:31 -0400

    ,,,

--- js/header.js
+++ js/header.js
@@ -6,22 +6,9 @@ window.addEventListener('resize', function() { setHeader(); });
 function setHeader() {
     let currentTheme = Load(_.DisplayTheme);
 
-    let desktop = window.innerWidth > 700;
-
     document.getElementById("side").innerHTML = '<div id="headerbg"></div>';
-
-    if (desktop) {
-        document.getElementById("headerbg").innerHTML = '<div class="titlebv"><a href="home.html"></a></div>';
-
-        if (currentTheme === Themes.Light) {
-            document.querySelector('.titlebv').className = 'titlewv';
-        }
-
-    } else {
-        document.getElementById("headerbg").innerHTML = '<div class="titlebh"><a href="home.html"></a></div>';
-
-        if (currentTheme === Themes.Light) {
-            document.querySelector('.titlebv').className = 'titlewh';
-        }
+    document.getElementById("headerbg").innerHTML = '<div class="titlebh"><a href="home.html"></a></div>';
+    if (currentTheme === Themes.Light) {
+        document.querySelector('.titlebh').className = 'titlewv';
     }
 }
