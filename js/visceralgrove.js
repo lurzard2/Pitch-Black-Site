01e930874b87d7faad135df3b8189926e2749466
lur <lurienthelizard@gmail.com>
2026-09-07 13:25:06 -0400

    misc

--- js/visceralgrove.js
+++ js/visceralgrove.js
@@ -4,10 +4,10 @@ const __ = Load(_.VisceralGrove);
 function _Save() { Save(_.VisceralGrove, __); }
 
 const path = "https://lurvixen.com/visceralgrove";
-function GetChapter(dest){
+export function GetChapter(dest){
     return window.location.href === path + dest;
 }
-function SetChapter(dest = ""){
+export function SetChapter(dest = ""){
     // Start game flag to change index
     if (GetChapter(2)){
         console.log("ENTER");
