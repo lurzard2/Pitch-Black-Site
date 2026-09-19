ddf09db8ed97c9e90aaf09092c2a57c8c4bfc7c7
lur <lurienthelizard@gmail.com>
2026-09-18 22:06:30 -0400

    misc

---
+++ visceralgrove/src/tiledParser.js
@@ -0,0 +1,16 @@
+export const JSONType = {
+    WORLD: 'world',
+    MAP: 'map',
+    TILESET: 'tileset'
+}
+export const MapLayerType = {
+    TILE: 'tilelayer',
+    OBJECT: 'objectgroup',
+    GROUP: 'group'
+}
+
+export class TiledParser {
+    constructor() {
+
+    }
+}
