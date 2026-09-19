85b3ff13c44fc6d29d0a000fc4e3bb6270146b5a
lur <lurienthelizard@gmail.com>
2026-09-19 00:15:37 -0400

    misc

--- visceralgrove/game.js
+++ visceralgrove/game.js
@@ -1,3 +1,29 @@
+/*
+
+Hi.
+Yes the site's 'savefile' is saved on the client.
+This is a singleplayer game and your progress is stored locally.
+
+By viewing the source code you acknowledge your experience will be permanently affected.
+Neither positive, nor negative,
+Just affected.
+
+These experiences are intended to be experienced BLIND.
+Viewing internal functioning could expose things otherwise not meant to be seen.
+Or possibly, hidden for you to find...
+I ask you wait until you are ready to proceed, to do this.
+
+BE WARY OF SPOILERS.
+DO NOT LEAK.
+DO NOT SABOTAGE.
+PLEASE ENJOY WHAT IS HERE IN A FAITHFUL WAY.
+AND HAVE FUN.
+THAT IS ALL.
+
+ */
+
+
+
 export const directory = '/visceralgrove/';
 export let debug = window.location.hostname === 'localhost';
 export const pixiDirectory = debug ? '' : '/visceralgrove/';
@@ -90,7 +116,7 @@ export function SlimeGridLoop(length, callback) {
         callback(i, new XYZ(x, y));
     }*/
 
-    let i = 0;
+    let i = -1;
     for (let y = 0; y < GRID.y; y++) {
         for (let x = 0; x < GRID.x; x++) {
             //console.log(`Tile index ${i} is located at (${x}, ${y})`);
