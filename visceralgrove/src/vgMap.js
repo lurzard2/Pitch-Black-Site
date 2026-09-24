8dbdb8ef76acdcb13e39db6bc998319a39d4c2a8
lur <lurienthelizard@gmail.com>
2026-09-24 09:59:23 -0400

    misc

--- visceralgrove/src/vgMap.js
+++ visceralgrove/src/vgMap.js
@@ -1,8 +1,10 @@
-import { MapInitializer } from './mapInitializer.js';
+import { MapInitializer } from './mapInitializer.js'
+import {debug} from '../game.js'
 
 export class VGMap {
     constructor(pos) {
         this.initMap = new MapInitializer(pos)
+        if (debug) { console.debug('VGMap initializing...', this.initMap) }
     }
 
     Render() {
